adefae0507297e4b6e315e5114c467aa09e7e1e1
bala-amypo <bala@amypo.in>
2025-12-22 09:21:03 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,16 +16,46 @@ public class SecurityConfig {
             .csrf().disable()
             .authorizeRequests()
             .anyRequest().permitAll();
+
         return http.build();
     }
+
     @Bean
-public AuthenticationManager authenticationManager(
-        AuthenticationConfiguration authenticationConfiguration
-) throws Exception {
-    return authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
 
-}
+
+
+// package com.example.demo.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+
+// @Configuration
+// public class SecurityConfig {
+
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//         http
+//             .csrf().disable()
+//             .authorizeRequests()
+//             .anyRequest().permitAll();
+//         return http.build();
+//     }
+//     @Bean
+// public AuthenticationManager authenticationManager(
+//         AuthenticationConfiguration authenticationConfiguration
+// ) throws Exception {
+//     return authenticationConfiguration.getAuthenticationManager();
+// }
+
+// }
 
 
 
