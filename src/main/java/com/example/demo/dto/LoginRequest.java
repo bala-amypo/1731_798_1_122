bc35c366470a9a4e646a7b684f6b5b4cf6b3b8a2
bala-amypo <bala@amypo.in>
2025-12-26 09:28:46 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,35 +1,3 @@
-// package com.example.demo.dto;
-
-// public class LoginRequest {
-//     private String email;
-//     private String password;
-
-//     // Default constructor
-//     public LoginRequest() {}
-
-//     // Parameterized constructor
-//     public LoginRequest(String email, String password) {
-//         this.email = email;
-//         this.password = password;
-//     }
-
-//     // Getters and setters
-//     public String getEmail() {
-//         return email;
-//     }
-
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
-
-//     public String getPassword() {
-//         return password;
-//     }
-
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
-// }
 package com.example.demo.dto;
 
 public class LoginRequest {
@@ -39,13 +7,13 @@ public class LoginRequest {
     // Default constructor
     public LoginRequest() {}
     
-    // Constructor with parameters
+    // Parameterized constructor
     public LoginRequest(String email, String password) {
         this.email = email;
         this.password = password;
     }
     
-    // Getters and setters
+    // Getters and Setters
     public String getEmail() {
         return email;
     }
